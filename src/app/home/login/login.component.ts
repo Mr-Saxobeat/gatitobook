62fd8cd267eb640497322e67cd819769bad60732
Weiglas Ribeiro <weiglasribeiro@gmail.com>
2021-09-13 09:11:34 -0300

    Add AutenticacaoService ao login.component

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AutenticacaoService } from 'src/app/autenticacao/autenticacao.service';
 
 @Component({
   selector: 'app-login',
@@ -9,14 +10,20 @@ export class LoginComponent implements OnInit {
   usuario: string = '';
   senha: string = '';
 
-  constructor() { }
+  constructor(private authService: AutenticacaoService) { }
 
   ngOnInit(): void {
   }
 
   login() {
-    console.log(this.usuario);
-    console.log(this.senha);
+    this.authService.autenticar(this.usuario, this.senha)
+      .subscribe(() => {
+        console.log('Autenticado com sucesso');
+      },
+      (error) => {
+        alert('Usuário ou senha inválida.');
+        console.error(error);
+      });
   }
 
 }
