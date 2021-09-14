4c6c8565b9be9c438735f340c2c85dca635e3519
Weiglas Ribeiro <weiglasribeiro@gmail.com>
2021-09-14 14:31:47 -0300

    Substitui subscribe pelo pipe

--- src/app/animais/lista-animais/lista-animais.component.ts
+++ src/app/animais/lista-animais/lista-animais.component.ts
@@ -1,7 +1,9 @@
+import { switchMap } from 'rxjs/operators';
 import { AnimaisService } from './../animais.service';
 import { UsuarioService } from './../../autenticacao/usuario/usuario.service';
 import { Animais } from './../animais';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-lista-animais',
@@ -10,7 +12,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaAnimaisComponent implements OnInit {
 
-  animais!: Animais;
+  animais$ !: Observable<Animais>;
 
   constructor(
     private usuarioService: UsuarioService,
@@ -18,14 +20,12 @@ export class ListaAnimaisComponent implements OnInit {
     ) { }
 
   ngOnInit(): void {
-    this.usuarioService.retornaUsuario()
-      .subscribe((usuario) => {
+    this.animais$ = this.usuarioService.retornaUsuario().pipe(
+      switchMap((usuario) => {
         const userName = usuario.name ?? '';
-        this.animaisService.listaDoUsuario(userName)
-          .subscribe((animais) => {
-            this.animais = animais;
-          })
+        return this.animaisService.listaDoUsuario(userName);
       })
+    )
   }
 
 }
