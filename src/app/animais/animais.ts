a293e5d8ba80deef288591219f5892605caabcd1
Weiglas Ribeiro <weiglasribeiro@gmail.com>
2021-09-14 10:35:17 -0300

    Cria interface animais

--- src/app/animais/animais.ts
+++ src/app/animais/animais.ts
@@ -1,2 +1,12 @@
-export interface Animais {
+export interface Animal {
+  id: number;
+  postDate: Date;
+  url: string;
+  description: string;
+  allowComment: boolean;
+  likes: number;
+  comments: number;
+  userId: number;
 }
+
+export type Animais = Array<Animal>;
