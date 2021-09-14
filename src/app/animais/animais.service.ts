94ac18ebecaf9983b4a964d3372a6bbcab36913c
Weiglas Ribeiro <weiglasribeiro@gmail.com>
2021-09-14 10:35:40 -0300

    Cria metodo listaDoUsuario

--- src/app/animais/animais.service.ts
+++ src/app/animais/animais.service.ts
@@ -1,9 +1,31 @@
+import { TokenService } from './../autenticacao/token.service';
+import { Animais } from './animais';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+const API = 'http://localhosta:3000';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AnimaisService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private tokenService: TokenService,
+  ) { }
+
+  listaDoUsuario(nomeDoUsuario: string): Observable<Animais> {
+    const token = this.tokenService.retornaToken();
+    const headers = new HttpHeaders().append(
+      'x-access-token', token
+    )
+    return this.http.get<Animais>(
+      `${API}/${nomeDoUsuario}/photos`,
+      {
+        headers,
+      }
+      );
+  }
 }
