83b99dc41188e0ec4cb66c5d40bb85063aba4b17
Weiglas Ribeiro <weiglasribeiro@gmail.com>
2021-09-15 09:47:59 -0300

    Adiciona metodo gravar() comentario

--- src/app/animais/detalhe-animal/comentarios/comentarios.component.ts
+++ src/app/animais/detalhe-animal/comentarios/comentarios.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { ComentariosService } from './comentarios.service';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { Comentarios } from './comentarios';
+import { startWith, switchMap, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-comentarios',
@@ -9,11 +12,32 @@ import { Comentarios } from './comentarios';
 })
 export class ComentariosComponent implements OnInit {
 
+  @Input() id!: number;
   comentarios$!: Observable<Comentarios>;
+  comentarioForm!: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private comentarioService: ComentariosService,
+  ) { }
 
   ngOnInit(): void {
+    this.comentarios$ = this.comentarioService.buscaComentario(this.id);
+    this.comentarioForm = this.formBuilder.group({
+      comentario: ['', [Validators.maxLength(300)]],
+    });
+  }
+
+  gravar(): void {
+    const comentario = this.comentarioForm.get('comentario')?.value ?? '';
+    this.comentarios$ = this.comentarioService.incluiComentario(this.id, comentario)
+      .pipe(
+        switchMap(() => this.comentarioService.buscaComentario(this.id)),
+        tap(() => {
+          this.comentarioForm.reset();
+          alert("O comentário foi salvo!");
+        })
+      );
   }
 
 }
