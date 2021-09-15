28840aa0555f93308b85ae75cc5d3edf060ff346
Weiglas Ribeiro <weiglasribeiro@gmail.com>
2021-09-15 13:51:30 -0300

    Adiciona metodos upload e gravaArquivo

--- src/app/animais/novo-animal/novo-animal.component.ts
+++ src/app/animais/novo-animal/novo-animal.component.ts
@@ -1,5 +1,8 @@
+import { AnimaisService } from './../animais.service';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-novo-animal',
@@ -13,13 +16,35 @@ export class NovoAnimalComponent implements OnInit {
   preview!: string;
   percentualConcluido!: number;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private animaisService: AnimaisService,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+    this.formularioAnimal = this.formBuilder.group({
+      file: ['', Validators.required],
+      description: ['', Validators.maxLength(300)],
+      allowComments: [true],
+    })
   }
 
   upload() {
+    const allowComments = this.formularioAnimal.get('allowComments')?.value ?? false;
+    const description = this.formularioAnimal.get('description')?.value ?? '';
 
+    this.animaisService.upload(description, allowComments, this.file).pipe(
+      finalize(() => this.router.navigate(['']))
+    );
+  }
+
+  gravaArquivo(arquivo: any): void {
+    const [file] = arquivo?.files;
+    this.file = file;
+    const reader = new FileReader();
+    reader.onload = (event:any) => (this.preview = event.target.result);
+    reader.readAsDataURL(file);
   }
 
 
